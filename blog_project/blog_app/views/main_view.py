7b238ce288c54c5881a44b1e425541e4574983ed
Nipesh <nipeshincbca2021@gmail.com>
2025-07-11 12:19:39 +0545

    edit operation

--- blog_project/blog_app/views/main_view.py
+++ blog_project/blog_app/views/main_view.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect,render, get_object_or_404
 from ..models import Blog
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 
 def index(request):
@@ -53,9 +54,55 @@ def create_blog(request):
 def single_page(request, id): 
     # blog = Blog.objects.get(id=id)
     blog = get_object_or_404(Blog, id=id)
-    print(blog)
+    # print(blog)
     return render(request,'main/single_page.html',{'blog':blog})
 
+# def edit_blog(request, id ):
+#     blog = get_object_or_404(Blog, id=id)
+#     if request.method == 'POST':
+#         title = request.POST.get('title')
+#         category = request.POST.get('category')
+#         image = request.FILES.get('image')
+#         description = request.POST.get('description')   # 1    # 4
+        
+#         if blog.author != request.user:
+#             return redirect('single',id=blog.id)
+#         else:       
+#             blog.title = title
+#             blog.image = image
+#             blog.category = category
+#             blog.description = description
+#             blog.save()
+#             return redirect('single', id=blog.id)
+    
+#     return render(request,'main/edit_blog.html',{'blog':blog})
 
 
+def edit_blog(request, id):
+    previous_data = Blog.objects.get(id=id) 
+    print(previous_data.author)
+    
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        category = request.POST.get('category')
+        image = request.FILES.get('image')
+        description = request.POST.get('description')
+        
+        if previous_data.author == request.user:
+            previous_data.title = title
+            previous_data.category = category
+            previous_data.description = description
+            if image:
+                previous_data.image = image
+            previous_data.save()
+            return redirect('single',id=previous_data.id) 
+        else:
+            messages.error(request,'You are not authorized to edit this Blog')
+            return redirect('single',id=previous_data.id) 
+                
+    # if previous_data.author == request.user:
+        
+        
+    return render(request,'main/edit_blog.html',{'blog':previous_data})
+    
 
